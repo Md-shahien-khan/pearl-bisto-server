f544255bfea23692560d77701d7a9209ae323ea3
Md Shahien Khan <shawonkn58@gmail.com>
2025-01-10 04:05:30 +0000

    fixed some code

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Middle Ware
 app.use(cors());
 app.use(express.json());
 // ${process.env.DB_USER}:${process.env.DB_PASS}
 
-// Part 1 adding cluster connection
+// Part 1 adding cluster connection from mongo
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5uoh0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
